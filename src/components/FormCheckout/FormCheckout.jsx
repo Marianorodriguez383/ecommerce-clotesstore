9d87a0c3ba101f1996fa51686c2b346e009fa23c
Marianorodriguez383 <106201716+Marianorodriguez383@users.noreply.github.com>
2023-05-16 21:25:08 -0300

    form order

--- src/components/FormCheckout/FormCheckout.jsx
+++ src/components/FormCheckout/FormCheckout.jsx
@@ -2,38 +2,52 @@ import { TextField, Button } from "@mui/material";
 import React from "react";
 import styles from "./FormCheckout.module.css";
 
-const FormCheckout = () => {
+const FormCheckout = ({ handleChange, handleSubmit, errors }) => {
   return (
     <div className={styles.formContainer}>
-      <div>
-        <TextField
-          className={styles.textInput}
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-        />
-      </div>
-      <div>
-        <TextField
-          className={styles.textInput}
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-        />
-      </div>
-      <div>
-        <TextField
-          className={styles.textInput}
-          id="outlined-basic"
-          label="Outlined"
-          variant="outlined"
-        />
-      </div>
-      <div className={styles.buttonContainer}>
-        <Button variant="contained" color="primary">
-          Comprar
-        </Button>
-      </div>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <TextField
+            className={styles.textInput}
+            id="outlined-basic"
+            label="Nombre"
+            name="nombre"
+            variant="outlined"
+            error={errors.nombre ? true : false}
+            helperText={errors.nombre}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            className={styles.textInput}
+            id="outlined-basic"
+            label="E-mail"
+            name="email"
+            variant="outlined"
+            error={errors.email ? true : false}
+            helperText={errors.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            className={styles.textInput}
+            id="outlined-basic"
+            label="Telefono"
+            name="telefono"
+            variant="outlined"
+            error={errors.telefono ? true : false}
+            helperText={errors.telefono}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button variant="contained" color="primary" type="">
+            Comprar
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/FormCheckout/FormCheckoutContainer.jsx
+++ src/components/FormCheckout/FormCheckoutContainer.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import FormCheckout from "./FormCheckout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { CartContext } from "../../context/CartContext";
+import { database } from "../../firebaseConfig";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 
 const FormCheckoutContainer = () => {
+  const { cart, getTotalPrice, clearCart } = useContext(CartContext);
+
+  const [orderId, setOrderId] = useState(null);
+
+  const checkoutFn = (info) => {
+    let total = getTotalPrice();
+
+    let infoOrder = {
+      buyer: info,
+      items: cart,
+      total: total,
+      date: serverTimestamp(),
+    };
+
+    const ordersCollection = collection(database, "orders");
+    addDoc(ordersCollection, infoOrder).then((res) => setOrderId(res.id));
+
+    cart.map((productos) =>
+      updateDoc(doc(database, "productos", productos.id), {
+        stock: productos.stock - productos.quantity,
+      })
+    );
+
+    clearCart();
+  };
+
+  const { handleSubmit, handleChange, errors } = useFormik({
+    initialValues: {
+      nombre: "",
+      email: "",
+      telefono: "",
+    },
+    onSubmit: checkoutFn,
+    validationSchema: Yup.object().shape({
+      nombre: Yup.string()
+        .required("Campo obligatorio")
+        .min(3, "El nombre debe tener como mínimo 3 caracteres"),
+      email: Yup.string()
+        .email("Debe ser un e-mail valido")
+        .required("Campo obligatorio"),
+      telefono: Yup.number().required("Campo obligatorio"),
+    }),
+    validateOnChange: false,
+  });
+
   return (
     <div>
-      <FormCheckout />
+      {orderId ? (
+        <h1>
+          Gracias por su compra ! El número de orden de compra es: {orderId}
+        </h1>
+      ) : (
+        <FormCheckout
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          errors={errors}
+        />
+      )}
     </div>
   );
 };
